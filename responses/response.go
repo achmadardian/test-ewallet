4d2854342c61c6fb826d9af10fd251b792a1682a
achmadardian <achmadardian37@gmail.com>
2025-05-16 17:56:43 +0700

    feat: add template api response pkg

---
+++ responses/response.go
@@ -0,0 +1,84 @@
+package responses
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type ApiResponse struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+	Errors  any    `json:"errros,omitempty"`
+}
+
+func Ok(c *gin.Context, data any, message ...string) {
+	msg := "success"
+	if len(message) > 0 {
+		msg = message[0]
+	}
+
+	c.JSON(http.StatusOK, ApiResponse{
+		Code:    http.StatusOK,
+		Message: msg,
+		Data:    data,
+	})
+}
+
+func Created(c *gin.Context, data any, message ...string) {
+	msg := "created"
+	if len(message) > 0 {
+		msg = message[0]
+	}
+
+	c.JSON(http.StatusCreated, ApiResponse{
+		Code:    http.StatusCreated,
+		Message: msg,
+		Data:    data,
+	})
+}
+
+func Updated(c *gin.Context, data any, message ...string) {
+	msg := "updated"
+	if len(message) > 0 {
+		msg = message[0]
+	}
+
+	c.JSON(http.StatusOK, ApiResponse{
+		Code:    http.StatusOK,
+		Message: msg,
+		Data:    data,
+	})
+}
+
+func Deleted(c *gin.Context, message ...string) {
+	msg := "deleted"
+	if len(message) > 0 {
+		msg = message[0]
+	}
+
+	c.JSON(http.StatusOK, ApiResponse{
+		Code:    http.StatusOK,
+		Message: msg,
+	})
+}
+
+func NotFound(c *gin.Context, message ...string) {
+	msg := "not found"
+	if len(message) > 0 {
+		msg = message[0]
+	}
+
+	c.JSON(http.StatusNotFound, ApiResponse{
+		Code:    http.StatusNotFound,
+		Message: msg,
+	})
+}
+
+func InternalServerError(c *gin.Context) {
+	c.JSON(http.StatusInternalServerError, ApiResponse{
+		Code:    http.StatusInternalServerError,
+		Message: "internal server error",
+	})
+}
