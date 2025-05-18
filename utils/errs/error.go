d0ff6a95e9db6c3d2356c6ece8ab6a26c7b40021
achmadardian <achmadardian37@gmail.com>
2025-05-18 23:04:31 +0700

    feat(utils): add new error: data not found

--- utils/errs/error.go
+++ utils/errs/error.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrValidationError  = errors.New("validation error")
 	ErrPhoneAlreadyUsed = errors.New("nomor telepon sudah terdaftar")
+	ErrDataNotFound     = errors.New("data tidak ditemukan")
 )
