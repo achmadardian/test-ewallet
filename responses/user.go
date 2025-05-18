4f682f5ccd4860861e3898d528963c9093c50fbb
achmadardian <achmadardian37@gmail.com>
2025-05-18 23:05:12 +0700

    feat(response): add user update response

--- responses/user.go
+++ responses/user.go
@@ -12,3 +12,11 @@ type UserResponse struct {
 	Address     string    `json:"address"`
 	Created_at  string    `json:"created_at"`
 }
+
+type UserUpdateRespons struct {
+	Id        uuid.UUID `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Address   string    `json:"address"`
+	UpdatedAt string    `json:"updated_at"`
+}
