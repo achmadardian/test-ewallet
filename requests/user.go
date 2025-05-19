b71d02c706a53c2f817d42261ecce633ac447c01
achmadardian <achmadardian37@gmail.com>
2025-05-19 23:59:12 +0700

    feat: add User Request validation

--- requests/user.go
+++ requests/user.go
@@ -6,21 +6,21 @@ import (
 
 type UserRequest struct {
 	Id          uuid.UUID `json:"-"`
-	FirstName   string    `json:"first_name" binding:"required"`
-	LastName    string    `json:"last_name" binding:"required"`
-	PhoneNumber string    `json:"phone_number" binding:"required"`
+	FirstName   string    `json:"first_name" binding:"required,lte=50"`
+	LastName    string    `json:"last_name" binding:"required,lte=50"`
+	PhoneNumber string    `json:"phone_number" binding:"required,lte=20"`
 	Address     string    `json:"address" binding:"required"`
-	Pin         string    `json:"pin" binding:"required"`
+	Pin         string    `json:"pin" binding:"required,lte=6"`
 }
 
 type UserUpdateRequest struct {
 	Id        uuid.UUID `json:"-"`
-	FirstName string    `json:"first_name" binding:"required"`
-	LastName  string    `json:"last_name" binding:"required"`
+	FirstName string    `json:"first_name" binding:"required,lte=50"`
+	LastName  string    `json:"last_name" binding:"required,lte=50"`
 	Address   string    `json:"address,omitempty"`
 }
 
 type UserLoginRequest struct {
-	PhoneNumber string `json:"phone_number"`
-	Pin         string `json:"pin"`
+	PhoneNumber string `json:"phone_number" binding:"required,lte=20"`
+	Pin         string `json:"pin" binding:"required,lte=6"`
 }
